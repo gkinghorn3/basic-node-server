8493807b99004235760f1bde816142116e5b45ea
Grant <gkinghorn3@icloud.com>
2023-06-20 11:38:59 +0100

    m

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const http = require('http');
 
 function handleRequest(request, response) {
+
+   if (request.url === '/currenttime') {
+        response
+   } else if (request.url ===) {
     response.statusCode = 200;
     response.end('<h1>Hello world</h1>');
-}
+    }
+
+};
+
+
 
 const server = http.createServer(handleRequest);
 
