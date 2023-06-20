f7ba973f5d65aedf33703fc02382f534f8f1e2b4
Grant <gkinghorn3@icloud.com>
2023-06-20 11:48:55 +0100

    new url brnach added

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const http = require('http');
 function handleRequest(request, response) {
 
    if (request.url === '/currenttime') {
-        response
-   } else if (request.url ===) {
+    response.statusCode = 200;
+    response.end('<h1>' + new Date().toISOString() + '</h1>');
+   } else if (request.url === '/') {
     response.statusCode = 200;
     response.end('<h1>Hello world</h1>');
     }
